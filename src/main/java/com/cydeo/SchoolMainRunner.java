1f9a401231db94cddfcdb0c5a873fe274b722acc
Alexander Bessonov <bessonovUSA@gmail.com>
2022-09-01 18:07:30 -0500

    created for SchoolService , @Override methods.

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -14,12 +14,12 @@ public class SchoolMainRunner {
     public static CRUDService<Parent> parentService;
     public static CRUDService<Student> studentService;
 
-  //  static {
-     //   SchoolMainRunner.schoolService = new SchoolService();
+    static {
+        SchoolMainRunner.schoolService = new SchoolService();
       // SchoolMainRunner.courseService = new CourseService();
       // SchoolMainRunner.parentService = new ParentService();
-     //  SchoolMainRunner.studentService = new StudentService();
-   // }
+      // SchoolMainRunner.studentService = new StudentService();
+    }
 
     public static void main(String[] args) {
 
@@ -27,18 +27,6 @@ public class SchoolMainRunner {
     }
     public  SchoolMainRunner(){}
 /*
-  public Practice(CountryService countryService, DepartmentService departmentService,
-                    EmployeeService employeeService, JobHistoryService jobHistoryService,
-                    JobService jobService, LocationService locationService,
-                    RegionService regionService) {
-
-        Practice.countryService = countryService;
-        Practice.departmentService = departmentService;
-        Practice.employeeService = employeeService;
-        Practice.jobHistoryService = jobHistoryService;
-        Practice.jobService = jobService;
-        Practice.locationService = locationService;
-        Practice.regionService = regionService;
 
     }
  */
