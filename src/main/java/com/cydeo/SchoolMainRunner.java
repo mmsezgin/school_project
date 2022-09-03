2820d894ed5d7f158045c9380bae170cfe2aa3e5
Alexander Bessonov <bessonovUSA@gmail.com>
2022-09-03 18:53:25 -0500

    finished and tested: case 2

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.database.Database;
 import com.cydeo.entity.Course;
 import com.cydeo.entity.Parent;
 import com.cydeo.entity.School;
@@ -12,16 +13,16 @@ import java.util.UUID;
 
 public class SchoolMainRunner {
 
-   public static CRUDService<School> schoolService;
+    public static CRUDService<School> schoolService;
     public static CRUDService<Course> courseService;
     public static CRUDService<Parent> parentService;
     public static CRUDService<Student> studentService;
 
     static {
         SchoolMainRunner.schoolService = new SchoolService();
-     //  SchoolMainRunner.courseService = new CourseService();
-      // SchoolMainRunner.parentService = new ParentService();
-       //SchoolMainRunner.studentService = new StudentService();
+        //  SchoolMainRunner.courseService = new CourseService();
+        // SchoolMainRunner.parentService = new ParentService();
+        //SchoolMainRunner.studentService = new StudentService();
     }
 
     public static void main(String[] args) {
@@ -31,55 +32,77 @@ public class SchoolMainRunner {
         while (true) {
             System.out.println("Type id for selection");
             for (int i = 0; i < menuOptions().length; i++) {
-                System.out.println(i+" "+menuOptions()[i]);
+                System.out.println(i + " " + menuOptions()[i]);
             }
             int menuSelection = input.nextInt();
 
-            switch (menuSelection){
+            switch (menuSelection) {
                 case 0:         //add
                     System.out.println("please enter the new school name:");
 
                     String name = input.next();
 
                     System.out.println("please enter the new ID");
-                    int  l= input.nextInt();
-                    School schoolToAdd = new School(l,name);
+                    int l = input.nextInt();
+                    School schoolToAdd = new School(l, name);
                     schoolService.save(schoolToAdd);
 
                     break;
                 case 1:       //delete
                     System.out.println("Please enter the ID");
-                        schoolService.findAll().forEach(p-> System.out.println(p.id+" "+p.getName()));
-                       int id = input.nextInt();
-                       schoolService.deleteById(id);
+                    schoolService.findAll().forEach(p -> System.out.println(p.id + " " + p.getName()));
+                    int id = input.nextInt();
+                    schoolService.deleteById(id);
 
                     break;
-//                    case 2:  //update****
-//                        System.out.println("Please enter the ID");
-//                        schoolService.findAll().forEach(p-> System.out.println(p.id+" "+p.getName()));
-//                        int name2 = input.nextInt();
-//                        System.out.println();
-//                       School school = schoolService.findById(name2);
-//                        SchoolMainRunner.schoolService.update(school);
-//                        break;
-                case 3:
-                    System.out.println(schoolService.findAll());
-                    break;   //exit
-                case 4:
-                    System.exit(0);
-                    break;
-                    
+                case 2:  //update****
+                    System.out.println("Please enter the ID");
+                    schoolService.findAll().forEach(p -> System.out.println(p.id + " " + p.getName()));
+                    int schoolToUpdate = input.nextInt();
+                    System.out.println("What would you like to update: 1) name, 2) ID ");
+                    int toUpdate1or2 = input.nextInt();
+
+                    switch (toUpdate1or2) {
+                        case 1:
+                            System.out.println("Enter the School name:");
+                            String schoolnewName = input.next();
+                            School schoolNewName = Database.schoolList.stream()
+                                    .filter(school -> school.getId() == schoolToUpdate)
+                                    .findFirst().get();
+                            schoolNewName.setName(schoolnewName);
+
+                            schoolService.update(schoolNewName);
+                            break;
+                        case 2:
+                            System.out.println("enter the ID");
+                            int schoolNewId = input.nextInt();
+                            School schoolToScangeId = Database.schoolList.stream()
+                                    .filter(school -> school.getId() == schoolToUpdate)
+                                    .findFirst().get();
+
+                            schoolToScangeId.setId(schoolNewId);
+
+                            schoolService.update(schoolToScangeId);
+                            break;
+                    }
+                        break;
+                        case 3:
+                            System.out.println(schoolService.findAll());
+                            break;   //exit
+                        case 4:
+                            System.exit(0);
+                            break;
+
+                    }
             }
+
         }
 
-    }
-    public static String[] menuOptions(){
-        return new String []{"Add","Delete","Update","View schools",
+
+    public static String[] menuOptions () {
+        return new String[]{"Add", "Delete", "Update", "View schools",
                 "Exit"};
     }
-
-/*
-
-    }
- */
 }
+
+
